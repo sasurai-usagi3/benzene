f311f3e288bf01427a32421bf0701fe44ca609eb
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-02-06 08:11:29 +0900

    feature: Matrixクラス作成

--- docs/application.js
+++ docs/application.js
@@ -3,3 +3,28 @@ window.addEventListener('load', () => {
 
   benzen.setAttribute('transform', 'translate(60, 60) scale(20, 20)');
 });
+
+class Matrix {
+  constructor(a, b, c, d, e, f, g, h, i) {
+    this._array = [a, b, c, d, e, f, g, h, i];
+  }
+
+  multiply(other) {
+    const thisArray = this._array;
+    const otherArray = other._array;
+    const newArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    for(let i = 0; i < 3; ++i) {
+      for(let j = 0; j < 3; ++j) {
+        newArray[i + 3 * j] = thisArray[3 * j] * otherArray[i] + thisArray[3 * j + 1] * otherArray[i + 3] + thisArray[3 * j + 2] * otherArray[i + 6];
+      }
+    }
+
+    // TODO: 個々の要素を呼び出さずに書きたい
+    return new Matrix(newArray[0], newArray[1], newArray[2], newArray[3], newArray[4], newArray[5], newArray[6], newArray[7], newArray[8]);
+  }
+
+  getElement(r, c) {
+    return this._array[3 * r + c];
+  }
+}
